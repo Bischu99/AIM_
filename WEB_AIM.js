2a94473b3c7a0346c115cda2a8281e6e93e31698
Bischu99 <kinggodsugeun@gmail.com>
2020-07-27 15:21:31 +0900

    0727
    0727

--- WEB_AIM.js
+++ WEB_AIM.js
@@ -1,7 +1,12 @@
 var time_time = 0;
+var gameStartCheck;
 
 function gameStart(){
-    for (var i = 0; i<50 ; i++){
+    
+    if (gameStartCheck){
+        return alert("실행 중 입니다.");
+    }
+    for (var i = 0; i<10 ; i++){
         var img = document.createElement("img");
         var rnd_Z = Math.round(Math.random() * 95);
         var rnd_X = Math.round(Math.random() * 95);
@@ -15,27 +20,37 @@ function gameStart(){
         document.getElementById("inTarget").appendChild(img);
     }
     console.log(document.getElementById("inTarget").getElementsByTagName("img").length);
-    
-    clear_time();
-    //clearInterval - 일정시간마다 반복하는 것을 중단
-
-}
-function clear_time (){
-    setInterval(time_set,1000);
+    gameStartCheck = true;
+    startInterval();
 }
 function reSet(){
-    clearInterval(setInterval);
-    document.getElementById("inTarget").removeChild("img");
+    clearInterval(time_set);
+    document.querySelector("#Time").value = "00 : 00 : 00";
+    document.querySelector("#inTarget").innerHTML ="";
+    time_time = hours = minute = second = 0;
+    gameStartCheck = false; 
+
 }
-function time_set(){
-    time_time += 1;
-    var hours = parseInt(time_time/3600);
-    var minute = parseInt(time_time%3600/60);
-    var second = time_time%3600%60;
-    
-    hours <= 9 ? hours ='0'+hours : hours; 
-    minute <= 9 ? minute ='0'+minute : minute;
-    second <= 9 ? second ='0'+second : second;
-    document.querySelector("#Time").value = hours+" : "+minute+" : "+second;
-    
+function startInterval(){
+    time_set = setInterval(function(){
+        time_time += 1;
+        var hours = parseInt(time_time/3600);
+        var minute = parseInt(time_time%3600/60);
+        var second = time_time%3600%60;
+
+        hours <= 9 ? hours ='0'+hours : hours; 
+        minute <= 9 ? minute ='0'+minute : minute;
+        second <= 9 ? second ='0'+second : second;
+        document.querySelector("#Time").value = hours+" : "+minute+" : "+second;
+
+        if (document.getElementById("inTarget").getElementsByTagName("img").length == 0 )
+        {
+            clearInterval(time_set);
+            time_time = hours = minute = second = 0;
+            gameStartCheck = false; 
+            return clearInterval(time_set);
+        }
+
+
+    },1000);
 }
